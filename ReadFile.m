087156ed1c84148b32ff449408edb4b0542cdb58
Niels Van Gorp <vangorpniels@gmail.com>
2022-03-31 10:16:41 +0200

    Header variable aangemaakt

--- ReadFile.m
+++ ReadFile.m
@@ -3,7 +3,8 @@
 COMMANDS = regexp(fileread('PP01/S1_MVC_delt_links.txt'), '#', 'split');
 if isempty(COMMANDS{end}); COMMANDS(end) = []; end  %end of file correction
 DATA = char(COMMANDS(3));
-A = jsondecode(DATA)
+A = jsondecode(DATA);
+Header = A.x00_07_80_3B_46_63;
 
 %read raw data
 M = readmatrix('PP01/S1_MVC_delt_links.txt','Range',4);
