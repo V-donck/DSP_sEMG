cbe175ed53f36ab58dcd4aed03b9341f58796309
V-donck <tv26363@gmail.com>
2022-03-31 10:14:26 +0200

    added readFile

--- ReadFile.m
+++ ReadFile.m
@@ -1,4 +1,10 @@
+
+%read header
 COMMANDS = regexp(fileread('PP01/S1_MVC_delt_links.txt'), '#', 'split');
 if isempty(COMMANDS{end}); COMMANDS(end) = []; end  %end of file correction
 DATA = char(COMMANDS(3));
 A = jsondecode(DATA)
+
+%read raw data
+M = readmatrix('PP01/S1_MVC_delt_links.txt','Range',4);
+M(:,8)=[]
