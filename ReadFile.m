3f3bd633af87a63d624061d5237867024506d2c3
Niels Van Gorp <vangorpniels@gmail.com>
2022-03-31 10:18:12 +0200

    Resolution added

--- ReadFile.m
+++ ReadFile.m
@@ -4,7 +4,7 @@ COMMANDS = regexp(fileread('PP01/S1_MVC_delt_links.txt'), '#', 'split');
 if isempty(COMMANDS{end}); COMMANDS(end) = []; end  %end of file correction
 DATA = char(COMMANDS(3));
 A = jsondecode(DATA);
-Header = A.x00_07_80_3B_46_63;
+Resolution = A.x00_07_80_3B_46_63.resolution;
 
 %read raw data
 M = readmatrix('PP01/S1_MVC_delt_links.txt','Range',4);
