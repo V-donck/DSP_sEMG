4c228328ddc84de5663b1a5e2cc44c15f6ec199b
V-donck <tv26363@gmail.com>
2022-03-31 10:29:33 +0200

    added fft, and changed transformto_mV

--- ReadFile.m
+++ ReadFile.m
@@ -4,8 +4,16 @@ COMMANDS = regexp(fileread('PP01/S1_MVC_delt_links.txt'), '#', 'split');
 if isempty(COMMANDS{end}); COMMANDS(end) = []; end  %end of file correction
 DATA = char(COMMANDS(3));
 A = jsondecode(DATA);
-Resolution = A.x00_07_80_3B_46_63.resolution;
+resolution = A.x00_07_80_3B_46_63.resolution;
 
 %read raw data
 M = readmatrix('PP01/S1_MVC_delt_links.txt','Range',4);
-M(:,8)=[]
+M(:,8)=[] % remove /n
+M(:,1:2)  = [] %remove seq en DI
+
+%transform raw data to mV
+M_mV =  transformTo_mV(M,resolution)
+
+
+%fft
+F = fft(M,[],1)
