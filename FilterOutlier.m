7cd0587bec9ef833829c0b966972846fac50b4a7
Niels Van Gorp <vangorpniels@gmail.com>
2022-05-05 09:01:57 +0200

    fixed filter outlier

--- FilterOutlier.m
+++ FilterOutlier.m
@@ -7,8 +7,9 @@ function [M_mV] = FilterOutlier(M_mV,numDerivation)
     end
     for j=1:n % kolommen
         for i= 1:m % rijen
-            if(M_mV(i,j) < (means(j) - numDerivation*stds(j)) || M_mV(i,j) > (mean(j) + numDerivation*stds(j)))
-                M_mV(i,j) = mean([M_mV(i-1,j), M_mV(i+1,j)]);
+            if(M_mV(i,j) < (means(j) - numDerivation*stds(j)) || M_mV(i,j) > (means(j) + numDerivation*stds(j)))
+                % M_mV(i,j) = mean([M_mV(i-1,j), M_mV(i+1,j)]);
+                M_mV(i,j) = 0;
             end
         end
     end
